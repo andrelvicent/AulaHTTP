489e82e47e5d25d7c093515b166c4f3ab7bf9a04
André Luiz Vicente <andre.vicente@zup.com.br>
2021-10-13 17:01:12 -0300

    Configurar banco de dados Postgres

---
+++ src/main/java/online/andresamurai/gestaoprodutos/Repository/ProdutoRepository.java
@@ -0,0 +1,8 @@
+package online.andresamurai.gestaoprodutos.Repository;
+
+import online.andresamurai.gestaoprodutos.Models.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
+}
