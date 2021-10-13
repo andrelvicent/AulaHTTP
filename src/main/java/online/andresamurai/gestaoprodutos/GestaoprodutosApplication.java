8ff6e5656b9e01ebe410076c841bdcd1d3012582
André Luiz Vicente <andre.vicente@zup.com.br>
2021-10-13 15:18:44 -0300

    Primeiro commit

---
+++ src/main/java/online/andresamurai/gestaoprodutos/GestaoprodutosApplication.java
@@ -0,0 +1,13 @@
+package online.andresamurai.gestaoprodutos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoprodutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoprodutosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/online/andresamurai/gestaoprodutos/GestaoprodutosApplicationTests.java
@@ -0,0 +1,13 @@
+package online.andresamurai.gestaoprodutos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoprodutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
