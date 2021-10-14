6f3845a760817baa91758eb0df4ebc041f23f84b
André Luiz Vicente <andre.vicente@zup.com.br>
2021-10-14 12:28:18 -0300

    bugfix swaggerConfig class

--- src/main/java/online/andresamurai/gestaoprodutos/Config/SwaggerConfig.java
+++ src/main/java/online/andresamurai/gestaoprodutos/Config/SwaggerConfig.java
@@ -1,7 +1,6 @@
 package online.andresamurai.gestaoprodutos.Config;
 
 import io.swagger.annotations.Api;
-import jdk.javadoc.doclet.Doclet;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
